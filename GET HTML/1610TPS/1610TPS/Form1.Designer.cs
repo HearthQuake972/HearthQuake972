82cd1105fa45190c6d05b9130a2fc852eead6af5
Nicola Antonelli <antonelli.nicola@franchettisalviani.net>
2021-10-19 11:33:24 +0200

    PROGETTO CARICATO

---
+++ GET HTML/1610TPS/1610TPS/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAnalizza = new System.Windows.Forms.Button();
+            this.txtSito = new System.Windows.Forms.TextBox();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.lblmail = new System.Windows.Forms.Label();
+            this.txtbho = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnReset.Enabled = false;
+            this.btnReset.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Lime;
+            this.btnReset.Location = new System.Drawing.Point(482, 5);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(109, 20);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "ESCI";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Visible = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "SITE NAME       -->";
+            // 
+            // btnAnalizza
+            // 
+            this.btnAnalizza.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAnalizza.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalizza.ForeColor = System.Drawing.Color.Lime;
+            this.btnAnalizza.Location = new System.Drawing.Point(367, 5);
+            this.btnAnalizza.Name = "btnAnalizza";
+            this.btnAnalizza.Size = new System.Drawing.Size(109, 20);
+            this.btnAnalizza.TabIndex = 10;
+            this.btnAnalizza.Text = "ANALIZZA";
+            this.btnAnalizza.UseVisualStyleBackColor = false;
+            this.btnAnalizza.Click += new System.EventHandler(this.btnAnalizza_Click);
+            // 
+            // txtSito
+            // 
+            this.txtSito.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtSito.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSito.ForeColor = System.Drawing.Color.Lime;
+            this.txtSito.Location = new System.Drawing.Point(151, 5);
+            this.txtSito.Name = "txtSito";
+            this.txtSito.Size = new System.Drawing.Size(210, 18);
+            this.txtSito.TabIndex = 9;
+            // 
+            // txtmail
+            // 
+            this.txtmail.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtmail.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmail.ForeColor = System.Drawing.Color.Lime;
+            this.txtmail.Location = new System.Drawing.Point(151, 31);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(325, 18);
+            this.txtmail.TabIndex = 15;
+            this.txtmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblmail
+            // 
+            this.lblmail.AutoSize = true;
+            this.lblmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmail.Location = new System.Drawing.Point(12, 31);
+            this.lblmail.Name = "lblmail";
+            this.lblmail.Size = new System.Drawing.Size(133, 16);
+            this.lblmail.TabIndex = 16;
+            this.lblmail.Text = "NOTIFY EMAIL -->";
+            // 
+            // txtbho
+            // 
+            this.txtbho.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtbho.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbho.ForeColor = System.Drawing.Color.Lime;
+            this.txtbho.Location = new System.Drawing.Point(9, 68);
+            this.txtbho.Multiline = true;
+            this.txtbho.Name = "txtbho";
+            this.txtbho.Size = new System.Drawing.Size(889, 628);
+            this.txtbho.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(910, 708);
+            this.Controls.Add(this.txtbho);
+            this.Controls.Add(this.lblmail);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAnalizza);
+            this.Controls.Add(this.txtSito);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAnalizza;
+        private System.Windows.Forms.TextBox txtSito;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label lblmail;
+        private System.Windows.Forms.TextBox txtbho;
+
+    }
+}
+
---
+++ GET HTML/1610TPS/1610TPS/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAnalizza_Click(object sender, EventArgs e)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(txtSito.Text);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 80);
+
+            Socket client = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                string getRequest = "GET " + txtSito.Text + " HTTP/1.1\r\n" +
+                    "Host: " + ipAddress.ToString() + "\r\n" +
+                    "\r\n";
+
+                client.Connect(remoteEP);
+                client.Send(Encoding.ASCII.GetBytes(getRequest));
+                String page = "";
+                byte[] bytesRecived = new byte[1024];
+                int bytesCnt = 0;
+
+                do
+                {
+                    bytesCnt = client.Receive(bytesRecived, bytesRecived.Length, 0);
+                    page = page + Encoding.ASCII.GetString(bytesRecived, 0, bytesCnt);
+                } while (bytesCnt == bytesRecived.Length);
+
+                txtbho.Text = page;
+
+            }
+
+            catch { }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
